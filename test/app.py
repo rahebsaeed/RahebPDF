7293636edde2de83aad5c1cd5aa1920dbf36eca8
rahebsaeed <raheebareef@gmail.com>
2024-09-03 14:27:54 +0100

    1

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from my_pdf_html_lib.utils import extract_text_with_styles, convert_text_with_styles_to_html, save_html, convert_pdf_to_html
+from RahebPDF.utils import extract_text_with_styles, convert_text_with_styles_to_html, save_html, convert_pdf_to_html
 
 # Convert PDF to HTML and save it
 convert_pdf_to_html("sample.pdf", "output.html")
